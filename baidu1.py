10a06b5c59f369e0facde48e8fba90de8e7e25ed
Linzaiz <linzaiz@hotmail.com>
2020-12-21 07:46:55 +0800

    selenium.webdriver.ChromeOptions()的使用。

---
+++ baidu1.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+option = webdriver.ChromeOptions()
+option.binary_location = r"C:\Program Files (x86)\Google\Chrome Beta\Application\chrome.exe"
+
+browser = webdriver.Chrome(r"c:\tools\chromedriver.exe", chrome_options=option)
+try:
+    browser.get('https://www.baidu.com')
+    input = browser.find_element_by_id('kw')
+    input.send_keys('Python')
+    input.send_keys(Keys.ENTER)
+    wait = WebDriverWait(browser, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
+    print(browser.current_url)
+    print(browser.get_cookies())
+    print(browser.page_source)
+finally:
+    browser. close()
