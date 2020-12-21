849415be17a5a9b4728ce4bd9e0613f7766573a2
Linzai Z <linzaiz@hotmail.com>
2020-12-21 19:42:01 +0800

    selenium webdriver with proxy

--- baidu1.py
+++ baidu1.py
@@ -5,10 +5,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 option = webdriver.ChromeOptions()
-option.binary_location = r"C:\Program Files (x86)\Google\Chrome Beta\Application\chrome.exe"
-
+option.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+proxy = '111.217.10.40:80'  # 设置代理。这只是个例子，请换成有效的代理 
+option.add_argument(f'--proxy-server={proxy}') 
 browser = webdriver.Chrome(r"c:\tools\chromedriver.exe", chrome_options=option)
 try:
+#if 1==1:
     browser.get('https://www.baidu.com')
     input = browser.find_element_by_id('kw')
     input.send_keys('Python')
@@ -18,5 +20,8 @@ try:
     print(browser.current_url)
     print(browser.get_cookies())
     print(browser.page_source)
+
+except Exception as e:
+    print(e.message())
 finally:
     browser. close()
